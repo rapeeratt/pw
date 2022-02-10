c05c3dd551a4b2e85216e4fc32b077c3d85654f2
ratti222 <ratti222@gmail.com>
2022-02-10 14:41:05 +0700

    fix

--- push.js
+++ push.js
@@ -1,18 +1,24 @@
 var push = require("web-push");
 
 let vapiKeys = {
-  publicKey: 'BOkXeAtRnXWqfrNs9Cla1d4UoPjvUbhgYqyJm_OJHm7BU2Z0ZOpWNlyandmibawc0oV2axEGMiTZA7_goY4V4SU',
-  privateKey: 'r4xfZjXiS2o1NOUHGFMqWlyqM3lAJtp8s-hrR9nW2dY'
+    publicKey: 'BOkXeAtRnXWqfrNs9Cla1d4UoPjvUbhgYqyJm_OJHm7BU2Z0ZOpWNlyandmibawc0oV2axEGMiTZA7_goY4V4SU',
+    privateKey: 'r4xfZjXiS2o1NOUHGFMqWlyqM3lAJtp8s-hrR9nW2dY'
 };
 //let vapiKeys = push.generateVAPIDKeys();
 
 //console.log(vapiKeys);
 
 push.setVapidDetails(
-  'mailto:ratti222@gmail.com',
-  vapiKeys.publicKey,
-  vapiKeys.privateKey
+    'mailto:ratti222@gmail.com',
+    vapiKeys.publicKey,
+    vapiKeys.privateKey
 );
-//let sub = {};
+let sub = {
+        "endpoint": "https://fcm.googleapis.com/fcm/send/dAXDTfRJhng:APA91bGVoyiKAsm3GmteuAL1DHpNN1r6jM5pNup8ayRBTI6hSHsCFQyGDPYtUBFycIUlALdYjkyazcCKpqsbXjQx4enhkHvO5iImbkEA1SGNLR1zMblYHzqij4tbxTweC9rEfLB-P8Pt",
+        "expirationTime": null,
+        "keys": {
+            "p256dh": "BPXwHP5af4udIbONbCDulTebgo1kWc_6uQHhypVsDjNP1TE1-D1PUa1R_bAbON8KSO-FzL-OA3rbaZ_qMwcoBME",
+            "auth": "IvPOsHTAHbPRjaqDmZOJCg"
+        }};
 
-//push.sendNotification(sub, "Test message");
+        push.sendNotification(sub, "Test message");
--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener("push", () => {
-    self.registration.sendNotification('Test message', {})
+self.addEventListener("push", function(e) {
+    var options = {
+        body: 'This notification was generated from a push',
+        icon: 'images/example.png',
+        vibrate: '[100,50,100]',
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey = '2',
+        },
+        action: [{
+                action: 'explore',
+                title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.ServiceWorkerRegistration.showNotification('Hello world!', options)
+    );
 });
