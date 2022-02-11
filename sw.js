84b97dfaf0ae1ad0d027d1b7e902cfa0cc06f8e6
ratti222 <ratti222@gmail.com>
2022-02-11 15:27:22 +0700

    msg

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 console.log('Service worker loaded send push...');
 
 self.addEventListener('push', () => {
-     self.registration.showNotification('we received a message', {
+     self.registration.showNotification('we received a message abc', {
         body: 'Notify by me',
-        icon: './plus.ico'
+        icon: '/plus.ico'
     });
 
 });
