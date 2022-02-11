87d2522cc45504a94c73f7d9b9a433886f330924
ratti222 <ratti222@gmail.com>
2022-02-11 15:25:19 +0700

    icod

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 console.log('Service worker loaded send push...');
 
 self.addEventListener('push', () => {
-     self.registration.showNotification('Hello world!', {
+     self.registration.showNotification('we received a message', {
         body: 'Notify by me',
         icon: './plus.ico'
     });
